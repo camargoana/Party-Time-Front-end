d043ab834d3b2d8b77b43dd6b494fdf0a9143f08
Luis Guillermo Moreno <l.guillermo01@gmail.com>
2022-03-22 18:58:04 -0400

    prueba 22-03

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,14 +3,76 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top justify-content-evenly">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					Party Time
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div
+					className="navbar-right collapse navbar-collapse"
+				
+					id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<a
+								className="nav-link active"
+								aria-current="page"
+								href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								About
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Services
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Contact
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div
+					className="navbar-right collapse navbar-collapse"
+				
+					id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<a
+								className="nav-link active"
+								aria-current="page"
+								href="#">
+								Login
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Sign Up
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Log Out
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
